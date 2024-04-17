c4fc6f869496c666134b355202a5e48342bc971f
newalvaro9 <alvaropobladoresteban9@gmail.com>
2024-04-18 00:13:40 +0200

    custom configuration

--- src/main/java/me/newalvaro9/authmegreetings/AuthMeGreetings.java
+++ src/main/java/me/newalvaro9/authmegreetings/AuthMeGreetings.java
@@ -1,34 +1,47 @@
 package me.newalvaro9.authmegreetings;
 import me.newalvaro9.authmegreetings.listener.AuthMeListener;
 import me.newalvaro9.authmegreetings.service.AuthMeHook;
-
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class AuthMeGreetings extends JavaPlugin {
 
-    private Listener authMeListener;
-    private AuthMeHook authMeHook;
+    private static AuthMeGreetings plugin;
+
+    public Boolean isPublicJoinMessageEnabled;
+    public String publicJoinMessage;
+
+    public Boolean isPrivateJoinMessageEnabled;
+    public String privateJoinMessage;
+
+    public Listener authMeListener;
+    public AuthMeHook authMeHook;
 
     @Override
     public void onEnable() {
+        plugin = this;
+
         final PluginManager pluginManager = getServer().getPluginManager();
+
+        // BEFORE REGISTERING THE AUTHME COMPONENTS
+        saveDefaultConfig();
+        loadConfiguration();
 
         authMeHook = new AuthMeHook();
 
         if (pluginManager.isPluginEnabled("AuthMe")) {
             registerAuthMeComponents();
         }
-        getLogger().info("Auth Me Greetings has been successfully enabled!");
+
+        getLogger().info("AuthMe Greetings has been successfully enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Unhooking from AuthMe");
         authMeHook.killAuthMeHook();
-        getLogger().info("Disabling Auth Me Greetings");
+        getLogger().info("Disabling AuthMe Greetings");
     }
 
     /**
@@ -39,8 +52,21 @@ public class AuthMeGreetings extends JavaPlugin {
         getLogger().info("Hooking into AuthMe");
         authMeHook.initializeAuthMeHook();
         if (authMeListener == null) {
-            authMeListener = new AuthMeListener();
+            authMeListener = new AuthMeListener(isPublicJoinMessageEnabled, publicJoinMessage, isPrivateJoinMessageEnabled, privateJoinMessage);
             getServer().getPluginManager().registerEvents(authMeListener, this);
+        }
+    }
+
+    public void loadConfiguration() {
+        reloadConfig();
+
+        if (getConfig().contains("welcome_message")) {
+            isPublicJoinMessageEnabled = getConfig().getBoolean("welcome_message.enable_public_message");
+            isPrivateJoinMessageEnabled = getConfig().getBoolean("welcome_message.enable_private_message");
+            publicJoinMessage = getConfig().getString("welcome_message.public_message");
+            privateJoinMessage = getConfig().getString("welcome_message.private_message");
+        } else {
+            throw new IllegalArgumentException("Missing welcome_message properties [AuthMeGreetings/config.yml].");
         }
     }
 }
--- src/main/java/me/newalvaro9/authmegreetings/listener/AuthMeListener.java
+++ src/main/java/me/newalvaro9/authmegreetings/listener/AuthMeListener.java
@@ -2,18 +2,37 @@ package me.newalvaro9.authmegreetings.listener;
 
 import fr.xephi.authme.events.LoginEvent;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class AuthMeListener implements Listener {
 
-    String joinMessage = ChatColor.translateAlternateColorCodes('&',
-            "Welcome %name%");
+    public Boolean isPublicJoinMessageEnabled;
+    public String publicJoinMessage;
+
+    public Boolean isPrivateJoinMessageEnabled;
+    public String privateJoinMessage;
+
+    public AuthMeListener(Boolean isPublicJoinMessageEnabled, String publicJoinMessage, Boolean isPrivateJoinMessageEnabled, String privateJoinMessage) {
+        this.isPublicJoinMessageEnabled = isPublicJoinMessageEnabled;
+        this.publicJoinMessage = ChatColor.translateAlternateColorCodes('&', publicJoinMessage);
+
+        this.isPrivateJoinMessageEnabled = isPrivateJoinMessageEnabled;
+        this.privateJoinMessage = ChatColor.translateAlternateColorCodes('&', privateJoinMessage);
+    }
 
     @EventHandler
     public void onLogin(LoginEvent event) {
-        String message = joinMessage.replace("%name%", event.getPlayer().getName());
-        event.getPlayer().sendMessage(message);
+        if(isPublicJoinMessageEnabled) {
+            Player player = event.getPlayer();
+            Bukkit.broadcastMessage(publicJoinMessage.replace("%name%", player.getName()));
+        }
+        if(isPrivateJoinMessageEnabled) {
+            Player player = event.getPlayer();
+            player.sendMessage(privateJoinMessage.replace("%name%", player.getName()));
+        }
     }
 }
